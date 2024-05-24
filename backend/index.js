ccfcfa37816c61cbe9eadeb15dfeafed068c464e
Shrutika Bhilawade <43693750+shrutikambhilawade@users.noreply.github.com>
2024-05-24 18:16:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors(
+    {
+        origin: [],
+        methods: ["POST","GET"],
+        credentials:true
+    }
+))
 
 mongoose
     .connect(process.env.MONGO_URL, {
