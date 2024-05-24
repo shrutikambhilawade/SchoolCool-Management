8ec71381d0775bbde127a5e410e5598d12f8654d
Shrutika Bhilawade <43693750+shrutikambhilawade@users.noreply.github.com>
2024-05-24 18:35:02 +0530

    Update noticeHandle.js

--- frontend/src/redux/noticeRelated/noticeHandle.js
+++ frontend/src/redux/noticeRelated/noticeHandle.js
@@ -6,7 +6,7 @@ import {
     getError
 } from './noticeSlice';
 
-const REACT_APP_BASE_URL = "http://localhost:5000";
+const REACT_APP_BASE_URL = "https://school-cool-management-7xha.vercel.app/";
 
 export const getAllNotices = (id, address) => async (dispatch) => {
     dispatch(getRequest());
